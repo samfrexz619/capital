bbb871aa336f8b04de30edcbcc69f1a913b8f63d
samfrexz619 <samfrexz69@gmail.com>
2023-09-28 09:06:10 +0100

    feat: update background style

--- src/layout/Dashboard.tsx
+++ src/layout/Dashboard.tsx
@@ -1,6 +1,6 @@
 import { Outlet, useLocation } from "react-router-dom";
 import { useState } from 'react';
-import { Layout } from 'antd';
+import { Layout, ConfigProvider } from 'antd';
 import HeaderContent from "@/components/commons/HeaderContent";
 import SideBar from "@/components/commons/SideBar";
 
@@ -14,26 +14,36 @@ const Dashboard = () => {
   const [collapsed, setCollapsed] = useState(true);
 
   const { pathname } = useLocation()
-
+// EDF4FF
   return ( 
-    <Layout 
+    <ConfigProvider
+      theme={{
+        components: {
+          Layout: {
+            bodyBg: '#EDF4FF'
+          }
+        }
+      }}
     >
-      <SideBar 
-        pathname={pathname} 
-        collapsed={collapsed} 
-        setCollapsed={setCollapsed} 
-      />
-      <Layout className="site-layout" style={{ background: '#EDF4FF', marginLeft: '80px' }}>
-        <div className='container'>
-          <Header style={{ padding: 0, background: '#EDF4FF', height: '15vh' }}>
-            <HeaderContent  />
-          </Header>
-          <Content style={{ margin: '0' }}>
-            <Outlet  />
-          </Content> 
-        </div>
+      <Layout
+      >
+        <SideBar
+          pathname={pathname}
+          collapsed={collapsed}
+          setCollapsed={setCollapsed}
+        />
+        <Layout className="site-layout" style={{ background: '#EDF4FF', marginLeft: '80px' }}>
+          <div className='container'>
+            <Header style={{ padding: 0, background: '#EDF4FF', height: '15vh' }}>
+              <HeaderContent  />
+            </Header>
+            <Content style={{ margin: '0', minHeight: '100vh' }}>
+              <Outlet  />
+            </Content>
+          </div>
+        </Layout>
       </Layout>
-    </Layout>
+    </ConfigProvider>
    );
 }
  
